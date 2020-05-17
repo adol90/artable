aa06b3f48d5be5249208a28d58b80d8f54c9c3ee
AdelKazme <adol90@hotmail.com>
2020-05-17 23:09:35 +0300

    Initial Commit

---
+++ artable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  artable
+//
+//  Created by Adel Kazme on 24/09/1441 AH.
+//  Copyright © 1441 Adel Kazme. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
