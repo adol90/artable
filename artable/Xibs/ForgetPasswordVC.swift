85fca51fd54e9a6644a015ee2b9e7cb8886c425d
Adel Kazme <adelkazmy@gmail.com>
2020-05-20 03:25:07 +0300

    fixed auto layout for xib forget password file .

--- artable/Xibs/ForgetPasswordVC.swift
+++ artable/Xibs/ForgetPasswordVC.swift
@@ -18,7 +18,6 @@ class ForgetPasswordVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
     @IBAction func cancelPressed(_ sender: Any) {
